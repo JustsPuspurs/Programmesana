c096c7840ef88c8458b94574e782d1f8407bb58f
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2023-09-26 11:36:04 +0300

    task

---
+++ justsipb21/task/class.php
@@ -0,0 +1,46 @@
+<?php
+class Tasks
+{
+    private $conn;
+
+    private $table_name = "tasks";
+
+    public $id;
+    public $title;
+    public $description;
+    public $due_date;
+    public $status;
+    public $created_at;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function getAllTasks()
+    {
+        $query = "SELECT * FROM " . $this->table_name;
+        $result = $this->conn->query($query);
+
+        $tasks = array();
+
+        while ($row = $result->fetch_assoc()) {
+            $tasks[] = $row;
+        }
+
+        return $tasks;
+    }
+}
+// public function createTask($data){
+
+// }
+
+// public function updateTask($id, $data) {
+
+// }
+
+// public function deleteTask($id){
+
+
+// }
+?>
---
+++ justsipb21/task/jauns.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <?php
+        include "db.php";
+        include "class.php";
+    ?>
+</head>
+<body class = "body2">
+    <div class = "vidus">
+        
+    </div>
+</body>
+</html>
